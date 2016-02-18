d348d084df1b0840dcb425be8b6dc9d8c52dee6a
Kai-Chu Chung <cage.chung@gmail.com>
2016-02-18 16:51:32 +0800

    fix urlpatterns for admin site.

--- sandbox/urls.py
+++ sandbox/urls.py
@@ -1,8 +1,6 @@
 from django.conf.urls import patterns, include, url
-from django.conf.urls.i18n import i18n_patterns
 from django.conf import settings
 from django.contrib import admin
-from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.conf.urls.static import static
 
 from hooks.dashboard.app import application as hooks_dashboard
@@ -10,14 +8,18 @@ from oscar.app import application
 
 admin.autodiscover()
 
-urlpatterns = patterns(
-        (r'^admin/', include(admin.site.urls)),
-        url(r'^dashboard/hooks/', include(hooks_dashboard.urls)),
-        url(r'^i18n/', include('django.conf.urls.i18n')),
-        url(r'', include(application.urls)),
-)
+urlpatterns = [
+    url(r'^i18n/', include('django.conf.urls.i18n')),
+    url(r'^admin/', include(admin.site.urls)),
+    url(r'^dashboard/hooks/', include(hooks_dashboard.urls)),
+    url(r'', include(application.urls)),
+]
 
 if settings.DEBUG:
-    urlpatterns += staticfiles_urlpatterns()
-    urlpatterns += static(
-            settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    import debug_toolbar
+
+    # Server statics and uploaded media
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += patterns('',
+                            url(r'^__debug__/', include(debug_toolbar.urls)),
+                            )
